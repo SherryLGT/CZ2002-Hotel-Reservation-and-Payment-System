ec15e459917e81f9af589a0a9fff28e7be06bc8d
Nicole <nicoleliow@hotmail.com>
2016-03-31 11:51:43 +0800

    Added new room entities

--- src/entity/Room.java
+++ src/entity/Room.java
@@ -11,6 +11,8 @@ import java.io.Serializable;
 
 public class Room implements Serializable{
 	private int roomNo;
+	private String type;
+	private String price;
 	private String[] details;
 	private String status;
 	
@@ -18,6 +20,8 @@ public class Room implements Serializable{
 	
 	public Room(int roomNo, String[] details, String status) {
 		this.roomNo = roomNo;
+		this.type = type;
+		this.price = price;
 		this.details = details;
 		this.status = status;
 	}
@@ -30,6 +34,22 @@ public class Room implements Serializable{
 		this.roomNo = roomNo;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
 	public String[] getDetails() {
 		return details;
 	}
