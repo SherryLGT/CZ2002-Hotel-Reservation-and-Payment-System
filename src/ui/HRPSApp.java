8490a1ce6349e5728199cd444e2d72403cd63c86
Cherry Tan <Cherry-Crystal1995@hotmail.com>
2016-04-14 14:10:37 +0800

    Changed Occupancy Status name to Status Report

--- src/ui/HRPSApp.java
+++ src/ui/HRPSApp.java
@@ -23,7 +23,6 @@ public class HRPSApp {
 		MenuController menuControl = new MenuController();
 		
 		do {
-			System.out.print("\033[H\033[2J");
 			
 			HRPSApp.line("=", 68);
 			System.out.format("%1s %59s %6s %n", "|", "WELCOME TO HOTEL RESERVATION AND PAYMENT SYSTEM (HRPS)", "|");
@@ -33,7 +32,7 @@ public class HRPSApp {
 			System.out.format("%1s %16s %5s %43s %n", "|", "2.", "ROOM", "|");
 			System.out.format("%1s %16s %12s %36s %n", "|", "3.", "RESERVATION", "|");
 			System.out.format("%1s %16s %19s %29s %n", "|", "4.", "PAYMENT (CHECKOUT)", "|");
-			System.out.format("%1s %16s %17s %31s %n", "|", "5.", "OCCUPANCY STATUS", "|");
+			System.out.format("%1s %16s %17s %31s %n", "|", "5.", "STATUS REPORT", "|");
 			System.out.format("%1s %16s %5s %43s %n", "|", "0.", "EXIT", "|");
 			HRPSApp.line("=", 68);
 			
